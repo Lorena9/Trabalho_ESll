771f47fb3ef6382b6c356b0bdf2877012f0f514a
T <T@10.1.253.234>
2016-01-14 16:54:04 -0200

    Criação das classes.

---
+++ src/trabalho_es/Transacao.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabalho_es;
+
+import java.util.Date;
+
+/**
+ *
+ * @author T
+ */
+public class Transacao {
+    private Date data;
+    private double valor;
+}
---
+++ src/trabalho_es/Usuario.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabalho_es;
+
+import java.util.Date;
+
+/**
+ *
+ * @author T
+ */
+public class Usuario {
+   private String login;
+   private String senha;
+   private String nome;
+   private String endereco;
+   private String telefone;
+   private String cpf;
+   private Date dt_nascimento;
+   
+}
