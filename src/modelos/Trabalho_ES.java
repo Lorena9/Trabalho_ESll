cfab632f05f4b6e81fa2a2e9ff9a7ea77e56c4ec
carlosoliv <carlosoliv@sbuff-desktop.zentyal-domain.lan>
2016-01-28 13:12:39 -0200

    Teste git 2

--- src/modelos/Trabalho_ES.java
+++ src/modelos/Trabalho_ES.java
@@ -4,10 +4,6 @@
  * and open the template in the editor.
  */
 package modelos;
-
-import interfacegrafica.Login;
-import javax.swing.JFrame;
-// teste do git do carlos oliv
 /**
  *
  * @author Lorena
