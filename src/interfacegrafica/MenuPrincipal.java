374da600451c23c61b1cb1d7fe841c1846b28a27
T <T@Carlos_Ramon>
2016-02-16 16:26:15 -0200

    Tela e funcionalidade de Pagamento

--- src/interfacegrafica/MenuPrincipal.java
+++ src/interfacegrafica/MenuPrincipal.java
@@ -85,10 +85,13 @@ public class MenuPrincipal extends javax.swing.JFrame {
         jButton8 = new javax.swing.JButton();
         TelaPagamentos = new javax.swing.JPanel();
         jLabel5 = new javax.swing.JLabel();
-        jButton3 = new javax.swing.JButton();
+        pagar = new javax.swing.JButton();
         jLabel6 = new javax.swing.JLabel();
-        jTextField2 = new javax.swing.JTextField();
+        codigoPagamento = new javax.swing.JTextField();
         jButton4 = new javax.swing.JButton();
+        valorPagamento = new javax.swing.JTextField();
+        jLabel18 = new javax.swing.JLabel();
+        tipoPagamento = new javax.swing.JComboBox();
         TelaTransferencia = new javax.swing.JPanel();
         jLabel7 = new javax.swing.JLabel();
         jButton5 = new javax.swing.JButton();
@@ -113,7 +116,6 @@ public class MenuPrincipal extends javax.swing.JFrame {
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("ATM - ES II - Bem vindo!");
         setLocationByPlatform(true);
-        setPreferredSize(new java.awt.Dimension(400, 305));
         setResizable(false);
         getContentPane().setLayout(new java.awt.CardLayout());
 
@@ -511,7 +513,12 @@ public class MenuPrincipal extends javax.swing.JFrame {
         jLabel5.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
         jLabel5.setText("Pagamento");
 
-        jButton3.setText("Pagar");
+        pagar.setText("Pagar");
+        pagar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                pagarActionPerformed(evt);
+            }
+        });
 
         jLabel6.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
         jLabel6.setText("Código:");
@@ -523,27 +530,43 @@ public class MenuPrincipal extends javax.swing.JFrame {
             }
         });
 
+        jLabel18.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel18.setText("Valor:");
+
+        tipoPagamento.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        tipoPagamento.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Luz", "Água", "Telefone", "Internet", "Outros" }));
+
         javax.swing.GroupLayout TelaPagamentosLayout = new javax.swing.GroupLayout(TelaPagamentos);
         TelaPagamentos.setLayout(TelaPagamentosLayout);
         TelaPagamentosLayout.setHorizontalGroup(
             TelaPagamentosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, TelaPagamentosLayout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jButton3)
-                .addGap(175, 175, 175))
             .addGroup(TelaPagamentosLayout.createSequentialGroup()
-                .addGap(174, 174, 174)
-                .addComponent(jLabel5)
+                .addGroup(TelaPagamentosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(TelaPagamentosLayout.createSequentialGroup()
+                        .addGap(174, 174, 174)
+                        .addComponent(jLabel5))
+                    .addGroup(TelaPagamentosLayout.createSequentialGroup()
+                        .addGap(27, 27, 27)
+                        .addComponent(jButton4)))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, TelaPagamentosLayout.createSequentialGroup()
-                .addGap(0, 55, Short.MAX_VALUE)
+                .addContainerGap(71, Short.MAX_VALUE)
                 .addGroup(TelaPagamentosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jButton4)
-                    .addGroup(TelaPagamentosLayout.createSequentialGroup()
-                        .addComponent(jLabel6)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, TelaPagamentosLayout.createSequentialGroup()
+                        .addGroup(TelaPagamentosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(jLabel6, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(jLabel18, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                         .addGap(18, 18, 18)
-                        .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 295, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addGap(34, 34, 34))
+                        .addGroup(TelaPagamentosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(codigoPagamento, javax.swing.GroupLayout.PREFERRED_SIZE, 295, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGroup(TelaPagamentosLayout.createSequentialGroup()
+                                .addComponent(valorPagamento, javax.swing.GroupLayout.PREFERRED_SIZE, 116, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(78, 78, 78)
+                                .addComponent(tipoPagamento, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addGap(34, 34, 34))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, TelaPagamentosLayout.createSequentialGroup()
+                        .addComponent(pagar)
+                        .addGap(175, 175, 175))))
         );
         TelaPagamentosLayout.setVerticalGroup(
             TelaPagamentosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -552,13 +575,18 @@ public class MenuPrincipal extends javax.swing.JFrame {
                 .addComponent(jLabel5)
                 .addGap(49, 49, 49)
                 .addGroup(TelaPagamentosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(codigoPagamento, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel6))
-                .addGap(98, 98, 98)
-                .addComponent(jButton3)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 42, Short.MAX_VALUE)
+                .addGap(29, 29, 29)
+                .addGroup(TelaPagamentosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(valorPagamento, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel18)
+                    .addComponent(tipoPagamento, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(60, 60, 60)
+                .addComponent(pagar)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 38, Short.MAX_VALUE)
                 .addComponent(jButton4)
-                .addGap(32, 32, 32))
+                .addGap(22, 22, 22))
         );
 
         getContentPane().add(TelaPagamentos, "card3");
@@ -865,6 +893,19 @@ public class MenuPrincipal extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_valorSaqueActionPerformed
 
+    private void pagarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_pagarActionPerformed
+        String codigo = this.codigoPagamento.getText();
+        try {
+            Conta nova = new Saque(Integer.parseInt(this.valorPagamento.getText()) * 100, usuarioLogado.getConta()).sacar();
+            new ContaDAO().atualizaConta(nova);
+        } catch (SQLException ex) {
+            Logger.getLogger(MenuPrincipal.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (Exception ex) {
+            Logger.getLogger(MenuPrincipal.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        JOptionPane.showMessageDialog(null, "Pagamento de " + this.tipoPagamento.getSelectedItem()+" efetuado!");//opçao escolhida no
+    }//GEN-LAST:event_pagarActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel TelaCheque;
@@ -886,11 +927,11 @@ public class MenuPrincipal extends javax.swing.JFrame {
     private javax.swing.JButton botaoSaque;
     private javax.swing.JButton botaoTransferencias;
     private javax.swing.JButton botaoVoltarSaldo;
+    private javax.swing.JTextField codigoPagamento;
     private javax.swing.JButton jButton10;
     private javax.swing.JButton jButton11;
     private javax.swing.JButton jButton12;
     private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton3;
     private javax.swing.JButton jButton4;
     private javax.swing.JButton jButton5;
     private javax.swing.JButton jButton6;
@@ -910,6 +951,7 @@ public class MenuPrincipal extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel15;
     private javax.swing.JLabel jLabel16;
     private javax.swing.JLabel jLabel17;
+    private javax.swing.JLabel jLabel18;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
@@ -922,7 +964,6 @@ public class MenuPrincipal extends javax.swing.JFrame {
     private javax.swing.JPanel jPanel3;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTextArea jTextArea1;
-    private javax.swing.JTextField jTextField2;
     private javax.swing.JTextField jTextField3;
     private javax.swing.JTextField jTextField4;
     private javax.swing.JTextField jTextField5;
@@ -930,7 +971,10 @@ public class MenuPrincipal extends javax.swing.JFrame {
     private javax.swing.JToggleButton jToggleButton1;
     private javax.swing.JLabel mostraSaldo;
     private javax.swing.JLabel nomeUsuario;
+    private javax.swing.JButton pagar;
     private javax.swing.JButton sacarButton;
+    private javax.swing.JComboBox tipoPagamento;
+    private javax.swing.JTextField valorPagamento;
     private javax.swing.JTextField valorSaque;
     // End of variables declaration//GEN-END:variables
 }
