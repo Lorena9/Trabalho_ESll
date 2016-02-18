088894e9ecb53536a6f774c7cd1afd21860edf7b
barbara <barbaraborges@gmail.com>
2016-02-18 17:41:44 -0200

    Merge origin/master
    Conflicts:
    	cifra_decifra_rsa.c
    	nbproject/private/private.xml

---
+++ src/exceptions/SaldoInsuficienteException.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exceptions;
+
+/**
+ *
+ * @author Barbara
+ */
+public class SaldoInsuficienteException extends TransacaoException{
+    
+    public SaldoInsuficienteException(){
+        
+    }
+    
+    public SaldoInsuficienteException(String message){
+        super(message);
+    }
+}
