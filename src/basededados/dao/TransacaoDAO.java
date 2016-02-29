4a0d6218b93138547ffdad20ab67b2068a228196
barbara <barbaraborges@gmail.com>
2016-02-29 16:36:09 -0300

    Conserto da função voltar extrato período e SaqueDAO funcionando.

--- src/basededados/dao/TransacaoDAO.java
+++ src/basededados/dao/TransacaoDAO.java
@@ -11,7 +11,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Date;
+import java.sql.Date;
+import java.time.LocalDate;
 import modelos.Conta;
 import modelos.Transacao;
 
@@ -19,71 +20,17 @@ import modelos.Transacao;
  *
  * @author Barbara
  */
-public class TransacaoDAO {
+public abstract class TransacaoDAO {
 
     private Connection conn;
-    ResultSet resultado = null;
+    
 
     public TransacaoDAO() {
         conn = BancoDeDados.getInstance();
     }
+    
+    public abstract void nova(Conta conta, int valor) throws SQLException ;
 
-    public String getExtratoInicial(Conta conta) throws SQLException  {
-        String geraExtratoMes = "SELECT * FROM atm.transacao WHERE month(`data`) = month(now()) AND year(`data`)=year(now());";
-        PreparedStatement preparedStatement = conn.prepareStatement(geraExtratoMes);
-        resultado = preparedStatement.executeQuery();
-        
-        ArrayList<Transacao> extrato = new ArrayList<Transacao>();
-        
-        Transacao temp;
-        
-        Date dt_transacao;
-        int valor;
-        
-        while(resultado.next()){
-            dt_transacao  = resultado.getDate("data");
-            valor = resultado.getInt("valor");
-            
-            temp = new Transacao(dt_transacao, valor, conta);
-            
-            extrato.add(temp);
-        }
-        
-        String extratoTexto = "";
-        
-        for(int i=0; i < extrato.size(); i++){
-            extratoTexto = extratoTexto + extrato.get(i).toString() + "\n";
-        }
-        
-        return extratoTexto;
-    }
-
-    public ArrayList<Transacao> getExtrato(Conta conta, Date dt_ini, Date dt_fim) throws SQLException {
-        String geraExtrato = "SELECT * FROM transacao WHERE dt_transacao > ? AND dt_transacao < ?;";
-        PreparedStatement preparedStatement = conn.prepareStatement(geraExtrato);
-        preparedStatement.setDate(1, (java.sql.Date) dt_ini);
-        preparedStatement.setDate(2, (java.sql.Date) dt_fim);
-
-        resultado = preparedStatement.executeQuery();
-
-        ArrayList<Transacao> extrato = new ArrayList<Transacao>();
-
-        Transacao temp;
-
-        Date dt_transacao;
-        int valor;
-
-        while (resultado.next()) {
-
-            dt_transacao = resultado.getDate("dt_transacao");
-            valor = resultado.getInt("valor");
-
-            temp = new Transacao(dt_transacao, valor, conta);
-
-            extrato.add(temp);
-        }
-
-        return extrato;
-    }
+    
 
 }
